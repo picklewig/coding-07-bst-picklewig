41818d7d012c305258a17d62f433b28723d9db2c
Isaac Shepherd <isaac.shepherd1@yahoo.com>
2021-11-10 00:42:54 -0600

    listed out all methods in bintree.cpp, wrote out basic logic for public addNode method.

--- bintree.cpp
+++ bintree.cpp
@@ -4,4 +4,114 @@
 
 #include "bintree.h"
 
+BinTree::BinTree(){
+    *root = NULL;
+    count = 0;
+}
 
+BinTree::~BinTree(){
+    clear();
+}
+
+bool BinTree::isEmpty(){
+
+}
+
+int BinTree::getCount(){
+
+}
+
+bool BinTree::getRootData(Data *){
+
+}
+
+void BinTree::DisplayTree(){
+
+}
+
+//public overloaded methods
+
+void BinTree::clear(){
+
+}
+
+bool BinTree::addNode(int id, const string *info){
+    //validate info, make DataNode, pass to private addNode
+    bool added = false;
+    if(id > 0 and *info != ""){
+        DataNode* newNode = new DataNode;
+        newNode->data.id = id;
+        newNode->data.information = info;
+        if(addNode(newNode, root)){
+            added = true;
+            count++;
+        }
+    }
+    return added;
+}
+
+bool BinTree::removeNode(int){
+
+}
+
+bool BinTree::getNode(Data*, int){
+
+}
+
+bool BinTree::contains(int){
+
+}
+
+int BinTree::getHeight(){
+
+}
+
+void BinTree::displayPreOrder(){
+
+}
+
+void BinTree::displayPostOrder(){
+
+}
+
+void BinTree::displayInOrder(){
+
+}
+
+//private overloaded methods
+
+void BinTree::clear(DataNode*){
+
+}
+
+bool BinTree::AddNode(DataNode*, DataNode**){
+
+}
+
+DataNode* BinTree::removeNode(int, DataNode*){
+
+}
+
+bool BinTree::getNode(Data*, int, DataNode*){
+
+}
+
+bool BinTree::contains(int, DataNode*){
+
+}
+
+int BinTree::getHeight(DataNode*){
+
+}
+
+void BinTree::displayPreOrder(DataNode*){
+
+}
+
+void BinTree::displayPostOrder(DataNode*){
+
+}
+
+void BinTree::displayInOrder(DataNode*){
+
+}
